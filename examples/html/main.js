3af7ed81f33b4d3beee302ac723ed6004e1b3d82
meisterjustice <eziefulejustice@gmail.com>
2023-01-30 15:32:02 +0400

    chore: setup sidebar

--- examples/html/main.js
+++ examples/html/main.js
@@ -1,7 +1,7 @@
 import { LazerpayCheckout } from '@lazerpay-checkout/html'
 
 const paymentButton = document.getElementById('payment-button')
-
+new LazerpayCheckout()
 paymentButton.addEventListener('click', () => {
   const lazerpayCheckout = new LazerpayCheckout()
 })
